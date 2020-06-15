1c845cf1c5b0adf2bb8f107e6e9068cb24e99a1f
dongdong.lai <1037022907@qq.com>
2020-06-15 21:02:38 +0800

    在vscode里面开发，添加外部配置环境

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# deng
+# deng-cli
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="container">
+		<view class="cu-bar tabbar bg-white shadow foot">
+			<view class="action" @click="NavChange" data-cur="home">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/icon/home' + [PageCur=='home'?'Active':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='home'?'text-green':'text-gray'">首页</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="call">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/icon/call' + [PageCur == 'call'?'':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='call'?'text-green':'text-gray'">等号</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="my">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/icon/my' + [PageCur == 'my'?'Active':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='my'?'text-green':'text-gray'">我的</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				PageCur: 'home'
+			}
+		},
+		methods: {
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		
+	}
+</style>
